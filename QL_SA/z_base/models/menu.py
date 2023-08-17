81d34eef1c380b18ee9958180e5fb4f652adebe0
tuantruong01 <tuantruongtb01@gmail.com>
2023-08-17 21:40:32 +0700

    ud menu

--- QL_SA/z_base/models/menu.py
+++ QL_SA/z_base/models/menu.py
@@ -9,14 +9,25 @@ class Menu(models.Model):
     name = fields.Char(string=_('Tên thực đơn'), requied=True)
     dish_ids = fields.Many2many('tigo.dish', 'menu_dish_ref', 'menu_id', 'dish_id', string=_('Món ăn'))
     type_menu = fields.Selection([('draft', 'Suất ăn'), ('table', 'Bàn')], string=_('Kiểu thực đơn'), default="draft")
-    sate = fields.Selection([('draft', 'Chưa Kích Hoạt'), ('use', 'Kích hoạt')], string=_('Trạng Thái'), default="draft")
+    state = fields.Selection([('unactive', 'Chưa Kích Hoạt'), ('active', 'Kích hoạt')], string=_('Trạng Thái'),
+                            default="unactive")
+
 
 class SettingMenu(models.Model):
     _name = 'tigo.menu.setting'
     _description = 'Cấu Hình'
 
     id = fields.Integer(string=_('Mã thực đơn'), readonly=1)
-    sate = fields.Selection([('draft', 'Chưa kích hoạt'), ('use', 'Đã kích hoạt')], string=_('Trạng Thái'), default="draft")
+    state = fields.Selection([('unactive', 'Chưa kích hoạt'), ('active', 'Đã kích hoạt')], string=_('Trạng Thái'),
+                            default="unactive")
     menu_ids = fields.Many2many('tigo.menu', 'setting_menu_ref', 'setting_id', 'menu_id', string=_('Thực Đơn'))
-    day_start = fields.Datetime(string="Từ ngày", requied=True)
-    day_end = fields.Datetime(string="Đến ngày", requied=True)
+    day_start = fields.Date(string="Từ ngày", requied=True)
+    day_end = fields.Date(string="Đến ngày", requied=True)
+
+    def action_active(self):
+        for r in self:
+            r.state = "active"
+
+    def action_unactive(self):
+        for r in self:
+            r.state = "unactive"
