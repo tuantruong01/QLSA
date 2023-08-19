de418e7512342ecd18e4d29be7a9f0718b84366d
sonvt <gmo@runsystem.net>
2023-08-19 09:49:35 +0700

    â

--- QL_SA/z_base/models/register_employee.py
+++ QL_SA/z_base/models/register_employee.py
@@ -11,3 +11,15 @@ class RegisterEmployee(models.Model):
     number_phone = fields.Char(string=_('Số điện thoại'), related='code_id.mobile_phone')
     menu = fields.Many2many('tigo.dish', 'menu_ref', 'register_id', 'dish_register_id', string=_('Thực đơn'))
     dpm_id = fields.Many2one('hr.department', string="Phòng Ban", related='code_id.department_id')
+
+
+class Client(models.Model):
+    _name = 'tigo.register.client'
+    _description = 'Đăng ký Khách Hàng'
+
+    registration_id = fields.Many2one('tigo.mealregister', string=_('Đăng ký suất ăn'))
+    menu = fields.Many2many('tigo.dish', 'menu_client_ref', 'register_client_id', 'dish_client_id',
+                            string=_('Thực đơn'))
+    name_client = fields.Char(string=_('Tên khách hàng'))
+    phone_client = fields.Integer(string=_('Số điện thoại'))
+    note = fields.Char(string="Ghi chú")
