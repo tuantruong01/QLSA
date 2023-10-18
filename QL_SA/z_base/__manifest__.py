647f47ed03a94123a3d60e6d41be9dd8af015933
tuantruong01 <tuantruongtb01@gmail.com>
2023-10-18 21:48:26 +0700

    a

--- QL_SA/z_base/__manifest__.py
+++ QL_SA/z_base/__manifest__.py
@@ -17,20 +17,22 @@
         'views/confirm_dish.xml',
         'views/hr_employee_inherit_view.xml',
 
-        'views/menu_set.xml',
-        'views/menu_table.xml',
         'views/menu_setting_day.xml',
         'views/menu_setting_week.xml',
-        'views/week.xml',
-        'views/dish.xml',
-        'views/product_product_inherit_views.xml',
 
         'views/service.xml',
         'views/quotes.xml',
         'views/pay_view.xml',
+
+        'views/menu_set.xml',
+        'views/menu_table.xml',
+        'views/week.xml',
+        'views/dish.xml',
+        'views/product_product_inherit_views.xml',
         'views/menu_service.xml',
         'views/room_service.xml',
         'views/popup.xml',
+        'views/view_setting.xml',
 
         'report/meal_register_report_view.xml',
         'report/menu_report.xml',
