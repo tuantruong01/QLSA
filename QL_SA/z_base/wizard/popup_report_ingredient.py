652165d88671a63cfabda9318d93fba4f4450924
tuantruong01 <tuantruongtb01@gmail.com>
2023-10-27 20:46:28 +0700

    a

--- QL_SA/z_base/wizard/popup_report_ingredient.py
+++ QL_SA/z_base/wizard/popup_report_ingredient.py
@@ -6,7 +6,17 @@ class PopupReportIngredient(models.TransientModel):
     _name = 'popup.report.ingredient'
     _description = 'Báo Cáo Nguyên Liệu'
 
-    categ_id = fields.Many2one('product.category', string='Nhóm Thực Phẩm', required=True)
+    categ_ids = fields.Many2many('product.category', 'product_category_ref', 'ingredient_id', 'category_id',
+                                 string='Nhóm Thực Phẩm', required=True)
+    all = fields.Boolean(string='Tất Cả')
 
     def action_print(self):
         return self.env.ref('z_base.report_menu_order').report_action(self)
+
+    @api.onchange('all')
+    def onchange_all(self):
+        for r in self:
+            if r.all == True:
+                r.categ_ids = self.env['product.category'].search([])
+
+
