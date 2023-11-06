0688d8ffd63ecadd524f4d43238133d863eba760
tuantruong01 <tuantruongtb01@gmail.com>
2023-11-06 16:50:18 +0700

    a

--- QL_SA/z_base/models/hr_employee.py
+++ QL_SA/z_base/models/hr_employee.py
@@ -7,7 +7,6 @@ class HrEmployeeInherit(models.Model):
     code_employee = fields.Char(string=_('Mã nhân viên'), readonly=1)
     company_id = fields.Many2one('res.company', string=_('Công ty'), default=lambda x: x.env.company)
 
-    _sql_constraints = [('code_employee', 'unique(code_employee)', 'Không Được Đặt Trùng Mã Nhân Viên')]
 
     @api.model
     def create(self, vals_list):
